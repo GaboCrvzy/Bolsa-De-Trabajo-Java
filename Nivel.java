d9749cd849f035d42a915524db1f9da9f60e5b13
GaboCrvzy <gabriel.fuentes.s@mail.pucv.cl>
2025-08-29 11:40:39 -0400

    Update Nivel.java

--- Nivel.java
+++ Nivel.java
@@ -1,5 +1,9 @@
 package bolsatrabajos;
 
-public enum Nivel{
-    BASICO, INTERMEDIO,AVANZADO   
+public enum Nivel {
+    BASICO, INTERMEDIO, AVANZADO;
+
+    public boolean esMayorOIgual(Nivel otro) {
+        return this.ordinal() >= otro.ordinal();
+    }
 }
