ad6f9da210cfe5ba74bab5108669928a57feb85b
GaboCrvzy <gabriel.fuentes.s@mail.pucv.cl>
2025-09-22 12:35:21 -0300

    Delete Nivel.java

--- Nivel.java
+++
@@ -1,9 +0,0 @@
-package bolsatrabajos;
-
-public enum Nivel {
-    BASICO, INTERMEDIO, AVANZADO;
-
-    public boolean esMayorOIgual(Nivel otro) {
-        return this.ordinal() >= otro.ordinal();
-    }
-}
